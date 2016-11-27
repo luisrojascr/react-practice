c56ed8002488bd019d665a36f33f51ab10059ddc
Luis Rojas <lsrojas.88@gmail.com>
2016-11-26 18:53:20 -0600

    adding files

---
+++ app.js
@@ -0,0 +1,35 @@
+var Comida = React.createClass({
+    handleLike: function(){
+        var nombreComida = this.props.nombre;
+        alert("Comida favorita: " + nombreComida);
+    },
+    render: function() {
+        return (
+            <div>
+                <h1 className="bg-success">{this.props.nombre}</h1>
+                <p className="bg-info">
+                    Comida <i>{this.props.children}</i>
+                </p>
+                <p>
+                    <span onClick={this.handleLike}
+                    className="glyphicon glyphicon-heart glyphicon-heart-lg red">
+                    </span>
+                </p>
+            </div>
+            );
+    }
+});
+
+ReactDOM.render(<div className="centerBlock">
+        <Comida nombre="Gallopinto">
+            Tica
+        </Comida>
+        <Comida nombre="Paella">
+            Española
+        </Comida>
+        <Comida nombre="Ceviche">
+            Peruana
+        </Comida>
+    </div>
+    , document.getElementById('container')
+);
